5a2601eb0fe09b2b366845338eb167a38dbdd30d
Max <mxcs02@gmail.com>
2022-12-23 03:49:49 +0200

    Add translate handler

--- src/translator/Translator.js
+++ src/translator/Translator.js
@@ -9,12 +9,33 @@ function swapLanguages(from, setFrom, to, setTo) {
     setTo(temp)
 }
 
+function translate(e, from, to, text, setTranslation) {
+    e.preventDefault();
+
+    const options = {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json',
+            'X-RapidAPI-Key': process.env.REACT_APP_API_KEY,
+            'X-RapidAPI-Host': process.env.REACT_APP_API_HOST
+        },
+        body: `{"text":"${text}","source":"${from}","target":"${to}"}`
+    };
+    
+    fetch(`https://${process.env.REACT_APP_API_HOST}/translate`, options)
+        .then(response => response.json())
+        .then(response => setTranslation(response.translations.translation))
+        .catch(err => console.error(err));
+}
+
 function Translator() {
     const [from, setFrom] = useState('en')
     const [to, setTo] = useState('uk')
-    
+    const [text, setText] = useState('')
+    const [translation, setTranslation] = useState('')
+
     return (
-        <form className='Translator container-md'>
+        <form onSubmit={e => translate(e, from, to, text, setTranslation)} className='Translator container-md'>
             <div className='d-flex flex-row mt-2'>
                 <Languages selected={from} onChange={e => setFrom(e.target.value)} />
                 <button type='button'
@@ -26,10 +47,10 @@ function Translator() {
             </div>
             <div className='row mt-0 g-2'>
                 <div className='col-lg'>
-                    <textarea className='form-control' rows='10' />
+                    <textarea value={text} onChange={e => setText(e.target.value)} className='form-control' rows='10' />
                 </div>
                 <div className='col-lg'>
-                    <textarea className='form-control h-100 Translator-result' readOnly placeholder='Translation' />
+                    <textarea value={translation} onChange={e => setTranslation(e.target.value)} className='form-control h-100 Translator-result' readOnly placeholder='Translation' />
                 </div>
             </div>
             <div className='row justify-content-end m-0 mt-2'>
