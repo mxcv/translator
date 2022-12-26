40232f06c1badf7b63f22acedb1c234e791f714e
Max <mxcs02@gmail.com>
2022-12-26 03:47:15 +0200

    Add language detection

--- src/languages/Languages.js
+++ src/languages/Languages.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import languages from './languages.json'
 
-function Languages({selected, onChange}) {
+function Languages({selected, onChange, hasDetect}) {
     const options = [];
     for (let l in languages)
         options.push(<option key={l} value={l}>{languages[l]}</option>)
-    return <select value={selected} onChange={onChange} className='form-select'>{options}</select>
+    options.unshift(hasDetect
+        ? <option key='' value=''>Detect language</option>
+        : <option key='' value='' disabled className='d-none'>Select target language</option>)
+        
+    return (
+        <select value={selected} onChange={onChange} required={!hasDetect} className='form-select'>
+            {options}
+        </select>
+    )
 }
 
 export default Languages
--- src/translator/Translator.js
+++ src/translator/Translator.js
@@ -1,52 +1,74 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import Languages from '../languages/Languages'
 import { ReactComponent as SwapIcon } from './swap.svg';
 import './Translator.css'
 
 function Translator() {
-    const [from, setFrom] = useState('en')
-    const [to, setTo] = useState('uk')
+    const [from, setFrom] = useState('')
+    const [to, setTo] = useState('')
     const [text, setText] = useState('')
     const [translation, setTranslation] = useState('')
     const [isLoading, setIsLoading] = useState(false)
+    const options = {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json',
+            'X-RapidAPI-Key': process.env.REACT_APP_API_KEY,
+            'X-RapidAPI-Host': process.env.REACT_APP_API_HOST
+        }
+    }
 
-    function swapLanguages() {
+    async function detectLanguage(text) {
+        options.body = `{"text":"${text}"}`
+        const response = await fetch(`https://${process.env.REACT_APP_API_HOST}/language_detect`, options)
+        if (!response.ok)
+            throw new Error(response)
+        return (await response.json()).language_detection.language
+    }
+
+    async function translate(text, from, to) {
+        options.body = `{"text":"${text}","source":"${from}","target":"${to}"}`
+        const response = await fetch(`https://${process.env.REACT_APP_API_HOST}/translate`, options)
+        if (!response.ok)
+            throw new Error(response)
+        return (await response.json()).translations.translation
+    }
+
+    function onSwapLanguages() {
         const temp = from
         setFrom(to)
         setTo(temp)
     }
     
-    function translate(e) {
-        e.preventDefault()
-        const options = {
-            method: 'POST',
-            headers: {
-                'content-type': 'application/json',
-                'X-RapidAPI-Key': process.env.REACT_APP_API_KEY,
-                'X-RapidAPI-Host': process.env.REACT_APP_API_HOST
-            },
-            body: `{"text":"${text}","source":"${from}","target":"${to}"}`
-        }
+    function onTranslate(e, detectedFrom) {
+        e?.preventDefault()
         setIsLoading(true)
-        fetch(`https://${process.env.REACT_APP_API_HOST}/translate`, options)
-            .then(response => response.json())
-            .then(response => setTranslation(response.translations.translation))
-            .catch(err => console.error(err))
-            .finally(() => setIsLoading(false))
+        if (from || detectedFrom)
+            translate(text, from ? from : detectedFrom, to)
+                .then(t => setTranslation(t))
+                .catch(err => console.error(err))
+                .finally(() => setIsLoading(false))
+        else
+            detectLanguage(text)
+                .then(language => {
+                    setFrom(language)
+                    onTranslate(null, language)
+                })
+                .catch(err => console.error(err))
     }
 
     return (
-        <form onSubmit={translate} className='Translator container-md'>
+        <form onSubmit={onTranslate} className='Translator container-md'>
             <div className='d-flex flex-row mt-2'>
-                <Languages selected={from} onChange={e => setFrom(e.target.value)} />
-                <button type='button' onClick={swapLanguages} className='btn btn-outline-secondary mx-2'>
+                <Languages selected={from} onChange={e => setFrom(e.target.value)} hasDetect />
+                <button type='button' onClick={onSwapLanguages} className='btn btn-outline-secondary mx-2'>
                     <SwapIcon />
                 </button>
                 <Languages selected={to} onChange={e => setTo(e.target.value)} />
             </div>
             <div className='row mt-0 g-2'>
                 <div className='col-lg'>
-                    <textarea value={text} onChange={e => setText(e.target.value)} className='form-control' rows='10' />
+                    <textarea value={text} onChange={e => setText(e.target.value)} required className='form-control' rows='10' />
                 </div>
                 <div className='col-lg'>
                     <textarea value={translation} onChange={e => setTranslation(e.target.value)} disabled={isLoading} className='form-control h-100 Translator-result' readOnly placeholder='Translation' />
