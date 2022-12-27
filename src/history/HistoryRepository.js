d4beb3f0f7b6907ebbe78fcf5d69dc2644b3a8f4
Max <mxcs02@gmail.com>
2022-12-27 05:31:59 +0200

    Add history repository

---
+++ src/history/HistoryRepository.js
@@ -0,0 +1,16 @@
+const key = 'translations'
+
+export function getTranslations() {
+    const translations = JSON.parse(localStorage.getItem(key))
+    return translations ? translations : []
+}
+
+export function saveTranslation(translation) {
+    const translations = getTranslations()
+    translations.push(translation)
+    localStorage.setItem(key, JSON.stringify(translations))
+}
+
+export function clearTranslations() {
+    localStorage.removeItem(key)
+}
