2ec0b72ec2a13ac769f6178405ebf4338b214cb5
Max <mxcs02@gmail.com>
2022-12-27 04:37:52 +0200

    Stringify json in api

--- src/translator/TranslatorRepository.js
+++ src/translator/TranslatorRepository.js
@@ -1,23 +1,24 @@
+const baseUrl = 'https://' + process.env.REACT_APP_API_HOST
 const options = {
     method: 'POST',
     headers: {
-        'content-type': 'application/json',
+        'Content-type': 'application/json',
         'X-RapidAPI-Key': process.env.REACT_APP_API_KEY,
         'X-RapidAPI-Host': process.env.REACT_APP_API_HOST
     }
 }
 
 export async function detectLanguage(text) {
-    options.body = `{"text":"${text}"}`
-    const response = await fetch(`https://${process.env.REACT_APP_API_HOST}/language_detect`, options)
+    options.body = JSON.stringify({ text: text })
+    const response = await fetch(baseUrl + '/language_detect', options)
     if (!response.ok)
         throw new Error(response)
     return (await response.json()).language_detection.language
 }
 
 export async function translate(text, from, to) {
-    options.body = `{"text":"${text}","source":"${from}","target":"${to}"}`
-    const response = await fetch(`https://${process.env.REACT_APP_API_HOST}/translate`, options)
+    options.body = JSON.stringify({ text: text, source: from, target: to })
+    const response = await fetch(baseUrl + '/translate', options)
     if (!response.ok)
         throw new Error(response)
     return (await response.json()).translations.translation
