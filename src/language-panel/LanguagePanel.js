575e83cc20af6acb78915f7db40582072f9f9eb9
Max <mxcs02@gmail.com>
2022-12-29 02:58:47 +0200

    Change swap button color

--- src/language-panel/LanguagePanel.js
+++ src/language-panel/LanguagePanel.js
@@ -3,10 +3,11 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 import LanguageSelect from '../language-select/LanguageSelect';
-import { ReactComponent as SwapIcon } from './swap.svg';
+import swap from './swap.svg';
+import './LanguagePanel.css'
 
 function LanguagePanel({from, setFrom, to, setTo}) {
-    function onSwapLanguages() {
+    function onSwap() {
         const temp = from
         setFrom(to)
         setTo(temp)
@@ -16,8 +17,8 @@ function LanguagePanel({from, setFrom, to, setTo}) {
         <Row className='mt-2 gx-2'>
             <Col><LanguageSelect selected={from} onChange={e => setFrom(e.target.value)} hasDetect /></Col>
             <Col xs='auto'>
-                <Button type='button' onClick={onSwapLanguages} variant='outline-secondary'>
-                    <SwapIcon />
+                <Button type='button' onClick={onSwap} variant='outline-secondary' className='btn-swap'>
+                    <img src={swap} alt='swap' />
                 </Button>
             </Col>
             <Col><LanguageSelect selected={to} onChange={e => setTo(e.target.value)} /></Col>
