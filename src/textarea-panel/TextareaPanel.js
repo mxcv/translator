27956d03a8dd0025298b5d1c12d59c2e949a2f49
Max <mxcs02@gmail.com>
2022-12-30 04:59:14 +0200

    Optimize textarea height

--- src/textarea-panel/TextareaPanel.js
+++ src/textarea-panel/TextareaPanel.js
@@ -8,40 +8,51 @@ function TextareaPanel({text, setText, translation, isLoading}) {
     const textRef = createRef()
     const translationRef = createRef()
 
-    function updateHeight(textarea) {
-        textarea.style.height = 'inherit';
-        const style = window.getComputedStyle(textarea);
+    function updateHeight(textarea, clone) {
+        textarea.style.height = 'inherit'
+        const style = window.getComputedStyle(textarea)
         const height = parseInt(style.getPropertyValue('border-top-width'), 10)
                      + parseInt(style.getPropertyValue('padding-top'), 10)
                      + textarea.scrollHeight
                      + parseInt(style.getPropertyValue('padding-bottom'), 10)
-                     + parseInt(style.getPropertyValue('border-bottom-width'), 10);
+                     + parseInt(style.getPropertyValue('border-bottom-width'), 10)
+                     + 'px'
     
-        textarea.style.height = `${height}px`;
+        textarea.style.height = height
+        if (clone)
+            clone.style.height = height
     }
-    
+
     useEffect(() => updateHeight(textRef.current), [textRef])
-    useEffect(() => updateHeight(translationRef.current), [translationRef])
+    useEffect(() => updateHeight(textRef.current, translationRef.current),
+        [translation]) // eslint-disable-line react-hooks/exhaustive-deps
+    
 
     return (
         <Row className='mt-0 g-2'>
-            <Col sm>
+            <Col md>
                 <Form.Control as="textarea"
-                    value={text}
                     ref={textRef}
+                    value={text}
                     onChange={e => setText(e.target.value)}
                     required
                     maxLength='5000'
                     className='Textarea' />
             </Col>
-            <Col sm>
+            <Col md>
                 <Form.Control as="textarea"
                     value={translation}
-                    ref={translationRef}
                     disabled={isLoading}
                     readOnly
                     placeholder='Translation'
-                    className='Textarea' />
+                    className='Textarea h-100 d-none d-md-block' />
+                <Form.Control as="textarea"
+                    ref={translationRef}
+                    value={translation}
+                    disabled={isLoading}
+                    readOnly
+                    placeholder='Translation'
+                    className='Textarea d-md-none' />
             </Col>
         </Row>
     )
